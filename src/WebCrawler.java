4b46027ec4957f909c3587e5595b3518dafb8bb0
Vidya Ramesh <ividya@stanford.edu>
2011-11-01 22:35:04 -0700

    Psuedocode for crawler and crawlerthread

--- src/WebCrawler.java
+++ src/WebCrawler.java
@@ -8,109 +8,45 @@ public class WebCrawler {
 	
 	static Vector<URL> urlsToSearch; 
 	static Vector<URL> urlsSearching;
+	static Vector<WebCrawlerThread> open_threads;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		WebCrawler w = new WebCrawler(); 
-		w.init();
-		w.crawl(); 
-
+		w.init(); 
 	}
 	
-	public void init() { 
-		//Load initial set of URLS
-		urlsToSearch = new Vector<URL>();
-		urlsSearching = new Vector<URL>(); 
-		URL init_url = null; 
-		try { 
-			init_url = new URL("http://google.com"); 
-		} catch (Exception e)
-		{
-			System.out.println(e.getMessage()); 
+	public static void updateUrlsToSearch(Vector<URL> newLinks) { 
+		lock(urlsToSearch); 
+		for (URL next : newLinks) { 
+			urlsToSearch.add(next); 
 		}
-		urlsToSearch.add(init_url); 		
-	}
-
-	public void addURL(URL my_url) { 
-		synchronized(urlsToSearch) { 
-			urlsToSearch.add(my_url); 
-		}
+		unlock(urlsToSearch); 
 	}
 	
-	public URL popURL() { 
-		URL returned_url = null; 
-		synchronized(urlsToSearch) { 
-			returned_url = urlsToSearch.remove(0); 
-		}
-		return returned_url; 
-	}
-	
-	public void crawl() { 
-		//urlsToSearch has been loaded
-		//for each of the urlsToSearch, span a new thread
+	public static void init() { 
 		while(true) { 
-			//new thread created
-			URL my_url = popURL(); 
-			if(my_url == null) { 
-				break; 
+			URL x = urlsSearching.remove(0); 
+			if(x == null) { 
+				lock(urlsSearching); 
+				lock(urlsToSearch);
+				for(URL next : urlsToSearch) { 
+					urlsSearching.add(next);
+				}
+				urlsToSearch.removeAllElements(); 
+				unlock(urlsSearching);
+				unlock(urlsToSearch);
+				continue; 
+				}
+			while(true) {
+				WebCrawlerThread new_thread = open_threads.remove(0); 
+				new_thread.my_url = x; 
+				new_thread.process(); 
+				open_threads.add(new_thread); 
 			}
-			WebCrawlerThread wct = new WebCrawlerThread(my_url); 
-			wct.findNextLinks(); 
 		}
-		/**
-		int i = 0; 
-		for(URL my_url : urlsToSearch) { 
-			try {
-				Vector<String> newLinks = (Vector<String>) SaveURL.extractLinks(my_url);
-				for(String new_url : newLinks) { 
-					if(new_url.startsWith("http")) { 
-						urlsSearching.add(new URL(new_url)); 
-						i += 1;
-					} else { 
-						new_url = my_url.toString() + new_url; 
-						urlsSearching.add(new URL(new_url)); 
-						i += 1; 
-					}
-				}
-				if(i >= 100) { 
-					System.out.println("reached 100"); 
-					break;
-				}
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} 
-		}
-		System.out.println(urlsSearching); 
-		for(URL my_url : urlsSearching) { 
-			System.out.println("------------------"); 
-			System.out.println(my_url); 
-			System.out.println("------------------"); 
-			try {
-				Vector<String> newLinks = (Vector<String>) SaveURL.extractLinks(my_url);
-				for(String new_url : newLinks) { 
-					if(new_url.startsWith("http")) { 
-						System.out.println(new_url); 
-						urlsToSearch.add(new URL(new_url)); 
-						i += 1;
-					} else { 
-						new_url = my_url.toString() + new_url; 
-						System.out.println(new_url); 
-						urlsToSearch.add(new URL(new_url)); 
-						i += 1; 
-					}
-				}
-				if(i >= 100) { 
-					System.out.println("reached 100"); 
-					break;
-				}
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} 
-		}**/
 	}
 
 }
--- src/WebCrawlerThread.java
+++ src/WebCrawlerThread.java
@@ -1,20 +1,36 @@
 import java.net.URL;
+import java.util.Vector;
 
 
 public class WebCrawlerThread extends Thread {
 
 	URL my_url;
+	Vector<URL> myNextLinks; 
 
 	public WebCrawlerThread(URL my_url) {
 		super();
 		this.my_url = my_url;
 	} 
 	
+	public WebCrawlerThread() {
+		super();
+	} 
+	
 	public void findNextLinks() { 
 		//request for url 
 		//get back HTML
 		//parse HTML for next links
-		//add links to WebCrawler 
+		//add links to myNextLinks 
+	}
+
+	public void process() {
+		openInBrowser(); 
+		findNextLinks(); 
+		WebCrawler.updateUrlsToSearch(myNextLinks);  
+	}
+	
+	public void openInBrowser() {
+		//open url in browser with special headers
 	}
 	
 }
